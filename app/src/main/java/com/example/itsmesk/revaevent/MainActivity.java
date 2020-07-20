fa3a2f918cc236bf9ef5b4b7763ce02c1c0b68f7
itsmeskhere <33190744+itsmeskhere@users.noreply.github.com>
2020-07-21 01:57:04 +0530

    Added

---
+++ app/src/main/java/com/example/itsmesk/revaevent/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.itsmesk.revaevent;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.itsmesk.revaevent.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button openTeachersActivityBtn,openUploadActivityBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+
+        openTeachersActivityBtn = findViewById ( R.id.openTeachersActivityBtn );
+        openUploadActivityBtn = findViewById ( R.id.openUploadActivityBtn );
+
+        openTeachersActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ItemsActivity.class);
+                startActivity(i);
+            }
+        });
+        openUploadActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, UploadActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
